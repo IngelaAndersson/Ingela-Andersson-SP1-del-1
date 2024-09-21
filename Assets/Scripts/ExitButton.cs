04ab57199fa2e913126472e9be5ea26ba1e934d4
Zoeylarsenmono <zoeylarsenmono@gmail.com>
2024-09-21 16:07:20 +0200

    exit (are you sure?)
    -La till frys när exit panel öppnar
    -are you sure you want to exit panel yes/no

--- Assets/Scripts/ExitButton.cs
+++ Assets/Scripts/ExitButton.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class ExitButton : MonoBehaviour
 {
+    [SerializeField] private GameObject AreYouSure;
    public void BackToMenu()
    {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+   public void showPanel()
+   {
+        AreYouSure.SetActive(true);
+        Time.timeScale = 0;
    }
+
+   public void closePanel()
+   {
+        AreYouSure.SetActive(false);
+        Time.timeScale = 1;
+    }
 }
